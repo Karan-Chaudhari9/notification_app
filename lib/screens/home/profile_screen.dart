8a6f6bd466fd6ef82900d8a6eb8dff9cbfab51f5
Karan-Chaudhari9 <110221397+Karan-Chaudhari9@users.noreply.github.com>
2023-09-28 22:35:29 +0530

    Update profile_screen.dart

--- lib/screens/home/profile_screen.dart
+++ lib/screens/home/profile_screen.dart
@@ -1,22 +1,7 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: ProfilePage(),
-    );
-  }
-}
-
-class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
 
   void _showSecondPage(BuildContext context) {
     Navigator.of(context).push(MaterialPageRoute(builder: (ctx) {
